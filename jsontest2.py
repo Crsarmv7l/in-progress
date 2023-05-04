398c1f225e8bed82135cba8962226abaf5742f64
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2023-05-03 20:11:13 -0400

    Add files via upload

--- jsontest2.py
+++ jsontest2.py
@@ -1,44 +1,70 @@
 import os 
 import time
 import json
+import subprocess
 
-def delete(n):
-    with open('test.json', 'r') as fr:
+file = 'test.json'
+
+def delete(i):
+    with open( file, 'r') as fr:
         lines = fr.readlines()
         
-        ptr = 1
+        ptr = i
         
-        with open('test.json', 'w') as fw:
+        with open(file, 'w') as fw:
             for line in lines:
-                if not ptr > n-1:
+                if not ptr == i:
                     fw.write(line)
-                ptr += 1
                 fw.close()
 
 def main():
+    #subprocess.Popen(['rtl_433', '-f', '433.92M', '-F', 'json:test.json'])
+    subprocess.Popen(['rtl_433', '-f', '315.00Mhz', '-R', '60', '-R', '90', '-R', '89', '-R', '82', '-R', '110', '-F', 'json:test.json'])
+    
     while True:
-        if os.stat("test.json").st_size == 0:
-            time.sleep (1)
+        if os.stat(file).st_size == 0:
+            time.sleep (3)
+            print("Waiting........")
         else:
                 dataList = []
-                with open('test.json') as f:
+                with open(file) as f:
                     for jsonObj in f:
                         dataDict = json.loads(jsonObj)
                         dataList.append(dataDict)
             
                         for data in dataList:
-                            i = 0
                             n = len(dataList)
-                            delete(n)
-                            while i < n:
+                            for i in range(n):
                                 data = dataList[i]
                                 if data["model"] == "PMV-107J":
-                                    print(data["id"])
+                                    print("Got Toyota: %s" % data["id"])
                                     del dataList[i]
+                                    delete(i)
+                                    n = n - 1
+                                elif data["model"] == "Citroen":
+                                    print("Got Citroen: %s" % data["id"])
+                                    del dataList[i]
+                                    delete(i)
+                                    n = n - 1
+                                elif data["model"] == "Ford":
+                                    print("Got Ford: %s" % data["id"])
+                                    del dataList[i]
+                                    delete(i)
+                                    n = n - 1
+                                elif data["model"] == "Renault":
+                                    print("Got Renault: %s" % data["id"])
+                                    del dataList[i]
+                                    delete(i)
+                                    n = n - 1
+                                elif data["model"] == "Schrader-SMD3MA4":
+                                    print("Got Schrader: %s" % data["id"])
+                                    del dataList[i]
+                                    delete(i)
                                     n = n - 1
                                 else:
-                                    print("Got Citroen")
+                                    print("Got Other")
                                     del dataList[i]
+                                    delete(i)
                                     n = n-1
     
 if __name__ == '__main__':
